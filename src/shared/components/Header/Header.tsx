008b56472e622a9ec7cfdbe7816d5e4f2e5b1668
Alexahder Kochish <shvepsolek@gmail.com>
2025-09-08 14:50:41 +0100

    widgets: add Header

---
+++ src/shared/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from '../ui/Button/Button'
+import s from './Header.module.css'
+import Image from 'next/image'
+import logo from '../../../../public/logo.svg'
+
+const Header = () => {
+    return (
+        <header className={s.header}>
+            <Image src={logo} width={104} height={26} alt="logo" />
+            <div className={s.buttonsGroup}>
+                <Button>Users</Button>
+                <Button>Sign Up</Button>
+            </div>
+        </header>
+    )
+}
+
+export default Header
